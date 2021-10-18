3bfa99bdb73844d3cfe542d5fd673ad18fbd2976
Ibifila <ibifilasworld@gmail.com>
2021-10-18 22:05:07 +0100

    coding the dashboard and poll pages

---
+++ script/polls.js
@@ -0,0 +1,50 @@
+// let pollinfo = [
+//     {
+//         serial: 1,
+//         polls: "Who is the fastest Goal scorer in the Champoins Leauge ?",
+//         categories: "Football",
+//         votes: "304k"
+//     },
+//     {
+//         serial: 2,
+//         polls: "Who is the fastest Goal scorer in the Champoins Leauge ?",
+//         categories: "Football",
+//         votes: "304k"
+//     },
+//     {
+//         serial: 3,
+//         polls: "Who is the fastest Goal scorer in the Champoins Leauge ?",
+//         categories: "Football",
+//         votes: "304k"
+//     },
+//     {
+//         serial: 4,
+//         polls: "Who is the fastest Goal scorer in the Champoins Leauge ?",
+//         categories: "Football",
+//         votes: "304k"
+//     },
+// ];
+
+// var polldata = document.getElementById("pollstable");
+// var html = "";
+
+
+// pollinfo.forEach(e =>{
+//     html +=`
+//         <tr>
+//             <td>${e.serial}</td>
+//             <td>${e.polls}</td>
+//             <td>${e.categories}</td>
+//             <td>304k</td>
+//             <td><span><a href="#"><img src="/dash-poll-images/trash-2.svg" alt=""></a></span>
+//                 <span><a href="#"><img src="/dash-poll-images/eye.svg" alt=""></a></span></td>
+//         </tr>
+//     `
+
+//     // console.log(e)
+//     polldata.innerHTML = html 
+//     console.log(html)
+// })
+
+
+
